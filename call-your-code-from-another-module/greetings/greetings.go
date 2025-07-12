1eaca08af55e7b856809796e5a93e58e53f8c039
Carlos Dourado <carlosismaelad@outlook.com>
2025-07-12 20:43:09 -0300

    using map for greetings to multiple people

--- call-your-code-from-another-module/greetings/greetings.go
+++ call-your-code-from-another-module/greetings/greetings.go
@@ -14,10 +14,23 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+func Hellos(names []string) (map[string] string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names{
+		message, err := Hello(name)
+		if err != nil{
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
 func randomFormat() string {
 	formats := []string{
 		"Hi, %v. Welcome!",
-		"Greet to see you, %v",
+		"Greet to see you, %v!",
 		"Hail, %v! Well met!",
 	}
 	return formats[rand.Intn(len(formats))]
--- call-your-code-from-another-module/hello/hello.go
+++ call-your-code-from-another-module/hello/hello.go
@@ -11,9 +11,11 @@ func main(){
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Carlos")
+	names := []string{"Carlos", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(message)
+	fmt.Println(messages)
 }
